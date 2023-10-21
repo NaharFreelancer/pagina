8aa2afa37b54aa5946694b7ade07faf3863ad5cc
NaharFreelancer <nahar.opediatech02@gmail.com>
2023-10-21 12:29:02 +0600

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,58 @@
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 2,
+//     spaceBetween: 16,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+    
+//     },
+//   });
+
+
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2,
-    spaceBetween: 16,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  slidesPerView: 2,
+  spaceBetween: 16,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is less than 768px
+    425: {
+      slidesPerView: 1,
     },
-  });
+  },
+  breakpoints: {
+    // when window width is less than 768px
+    375: {
+      slidesPerView: 1,
+    },
+  },
+});
+
+
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:false,
+  dots: false,
+  autoplay:true,
+  autoplayTimeout:2000,
+  stagePadding:50,
+  responsive:{
+      0:{
+          items:3
+      },
+      600:{
+          items:5
+      },
+      1000:{
+          items:8
+      }
+  }
+})
 
 
 // navbar fixed
@@ -21,4 +68,23 @@ var swiper = new Swiper(".mySwiper", {
       }
     });
   });
+
+
+// Gallary Slide
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
   
